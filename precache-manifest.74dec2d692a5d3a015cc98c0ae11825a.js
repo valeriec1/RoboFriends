628da9a9f8b8a6309eb6481843126d715fbb751f
valeriec1 <v123acs@gmail.com>
2020-03-20 12:41:04 -0500

    Updates

---
+++ precache-manifest.74dec2d692a5d3a015cc98c0ae11825a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb852e7cc928854de221af7c94319522",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/RoboFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "72e6fee743146279ddc3",
+    "url": "/RoboFriends/static/css/main.226de548.chunk.css"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/RoboFriends/static/js/2.58272216.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/RoboFriends/static/js/2.58272216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e6fee743146279ddc3",
+    "url": "/RoboFriends/static/js/main.a9b403ac.chunk.js"
+  },
+  {
+    "revision": "f684f813b251aeff87a7",
+    "url": "/RoboFriends/static/js/runtime-main.7c4013b2.js"
+  }
+]);
